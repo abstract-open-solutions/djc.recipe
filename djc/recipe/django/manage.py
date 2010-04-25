ca2aebf3cbeac076bab50da783780d6054d84a35
Simone Deponti <shywolf9982@gmail.com>
2010-04-26 01:35:33 +0200

    Slightly corrected manage script

--- djc/recipe/django/manage.py
+++ djc/recipe/django/manage.py
@@ -1,17 +1,20 @@
 import imp
-from django.core import management
+from django.core.management import setup_environ, ManagementUtility
 
 def main(settings_file):
+    import sys
     try:
         imp.acquire_lock()
         mod = imp.load_source('_django_settings', settings_file)
     except Exception, e:
         imp.release_lock()
-        import sys
         sys.stderr.write("Error loading the settings module '%s': %s"
                             % (settings_file, e))
         return sys.exit(1)
     
     imp.release_lock()
-    management.execute_manager(mod)
+
+    setup_environ(mod, '_django_settings')
+    utility = ManagementUtility(sys.argv)
+    utility.execute()
     
